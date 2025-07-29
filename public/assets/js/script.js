5940795a4a1b84410a5ca2519da39eadee478151
smjoy222 <smjoy222@gmail.com>
2025-07-30 00:00:51 +0600

    Home Page Design

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,60 +1,8 @@
-// navbar= document.getElementsByClassName("navbar");
-
-// for(i=0; i<navbar[0].children.length; i++) {
-//     navbar[0].children[i].addEventListener("click", function(event) {
-//         console.log(event.target.innerText);
-//     });
-// }
-
-// document.querySelectorAll("h3").forEach((varname) => {
-//     varname.onclick = function() {
-//         alert(this.innerHTML);
-//     }});
-// document.querySelectorAll("button").forEach((varname) => {
-//     varname.onclick = function() {
-//         document.querySelector(".change").innerHTML = 
-//         <p>hello this an inner html</p>;
-//         img = document.querySelector("img");
-//         img.src = "https://picsum.photos/200/300";
-//         this.classList.add("btn");
-//         img.classList.add("rounded");
-//     }});
-
-
-
-
-// let currentIndex = 0;
-// let maxwordIndex = words.length - 1;
-// words[currentWordIndex].style.opacity = "1";
-// let changeText = () => {
-//     let currentWord = words[currentIndex];
-//     let nextWord = currentWordIndex === maxwordIndex ? words[0] : words[currentIndex + 1];
-//     Array.from(currentWord.children).forEach((letter, i) => {
-//         setTimeout(() => {
-//             letter.className = "letter out";
-//         }, i * 80);
-//     });
-//     nextWord.style.opacity = "1";
-//         Array.from(nextWord.children).forEach((letter, i) => {
-//             letter.className = "letter behind";
-//             setTimeout(() => {
-//                 letter.className = "letter in";
-//             }, 340 + i * 80);
-//         });
-//     };
-
-//     changeText();
-//     setInterval(changeText, 3000);
-
-
-
-
-
 let words = document.querySelectorAll(".word");
 words.forEach((word) => {
     let letters = word.innerText.split("");
     word.textContent = "";
-    latters.forEach((letter) => {
+    letters.forEach((letter) => {
         let span = document.createElement("span");
         span.textContent = letter;
         span.className = "letter"; 
@@ -62,3 +10,30 @@ words.forEach((word) => {
     });
 });
 
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText, 3000);
